6b9587679bfcb6b943015e1f95991ea6aa7e0f1e
Tassilo Schweyer <dev@welterde.de>
2010-11-16 00:10:18 +0100

    added stub handshake handler

---
+++ src/minecraft/handlers/handshake.go
@@ -0,0 +1,21 @@
+package handshake
+
+import session "minecraft/session"
+import packets "minecraft/packets"
+import packet "minecraft/packets/handshake"
+
+func Handler(session *session.Session, msg packets.Packet) {
+	// try to cast to an Request
+	req, ok := msg.(packet.Request)
+	if ok != true {
+		panic("BUG!!11!!1: failed cast to Request.. something is very messed up!")
+	}
+	
+	// TODO: do an database lookup
+	
+	// yup.. answer handshake now
+	res := new(packet.Response)
+	res.hash = "-"
+	
+	session.Transmit(res)
+}
