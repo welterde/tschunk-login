67b203f4010d8175fc09ee81f19420afbfe533fb
Tassilo Schweyer <dev@welterde.de>
2010-11-20 12:12:59 +0100

    load logging configuration too

--- src/main.go
+++ src/main.go
@@ -1,8 +1,12 @@
 package main
 
 import server "minecraft/server"
+import log "log4go"
 
 func main() {
+	// load logging configuration
+	log.LoadConfiguration("etc/logging.xml")
+	
 	// create server
 	server, _ := server.Create("127.0.0.1:25565")
 
