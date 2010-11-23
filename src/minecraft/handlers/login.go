0aeeb6026f8b6e2839c8d653cf5040993add5cc9
Tassilo Schweyer <dev@welterde.de>
2010-11-24 00:12:08 +0100

    added login handler

---
+++ src/minecraft/handlers/login.go
@@ -0,0 +1,15 @@
+package login
+
+import session "minecraft/session"
+import packets "minecraft/packets"
+import packet  "minecraft/packets/login"
+
+func Handler(session *session.Session, msg packets.Packet) {
+	// try to cast to an request
+	_, ok := msg.(*packet.Request)
+	if ok != true {
+		panic("BUG!!11!!1: failed cast to Request.. something is very messed up!")
+	}
+	
+	// TODO: do something now...
+}
