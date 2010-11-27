645934a6beab1bd2914719bd96ded30966319fe5
Tassilo Schweyer <dev@welterde.de>
2010-11-27 23:25:21 +0100

    started implementing the entity manager this manager is created once per client => each client gets different entity id's

--- src/minecraft/entity.go
+++ src/minecraft/entity.go
@@ -4,7 +4,8 @@ import "sync"
 
 func NewEntityManager() (manager *EntityManager) {
 	manager = &EntityManager{
-		mapping: make(map[string]int32),
+		mapping:  make(map[string]int32),
+		iMapping: make([]int, 128),
 	}
 
 	return
@@ -13,20 +14,38 @@ func NewEntityManager() (manager *EntityManager) {
 
 type EntityManager struct {
 	mapping    map[string]int32
+	iMapping   []int
 	lock       sync.Mutex
 	currentPos int32
 }
 
-func (m *EntityManager) GetEntityID(uid string) {
+func (m *EntityManager) GetEntityID(uuid string) (eid int32) {
 	// lock this manager
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
-	// increment entity id by one
-	m.currentPos++
+	// search for it..
+	eid, ok = m.mapping[uuid]
+	if ok == nil {
+		return eid
+	} else {
+		// increment entity id by one
+		m.currentPos++
 
-	// create mapping
-	m.mapping[uid] = m.currentPos
+		// create mapping
+		m.mapping[uuid] = m.currentPos
 
-	// TODO: reverse mapping?
+		// reverse mapping
+		m.fixIMapping()
+		m.iMapping[m.currentPos] = uuid
+	}
+}
+
+
+func (m *EntityManager) fixIMapping() {
+	if m.currentPos >= cap(m.iMapping) {
+		newIM := make([]int, cap(m.iMapping)*2)
+		copy(newIM, m.iMapping)
+		m.iMapping = newIM
+	}
 }
