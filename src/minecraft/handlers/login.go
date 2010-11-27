c167a0b65f4dc2ef363a5f73652b0d5602d7ab58
Tassilo Schweyer <dev@welterde.de>
2010-11-27 23:24:34 +0100

    do nothing useful with the protocol version... ?

--- src/minecraft/handlers/login.go
+++ src/minecraft/handlers/login.go
@@ -6,10 +6,20 @@ import packet  "minecraft/packets/login"
 
 func Handler(session *session.Session, msg packets.Packet) {
 	// try to cast to an request
-	_, ok := msg.(*packet.Request)
+	req, ok := msg.(*packet.Request)
 	if ok != true {
 		panic("BUG!!11!!1: failed cast to Request.. something is very messed up!")
 	}
 
-	// TODO: do something now...
+	// check protocol version.. it should either be 5(minecraft client) or 7(manic digger)
+	if req.ProtocolVersion == 5 {
+		// minecraft client
+	} else if req.ProtocolVersion == 7 {
+		// manic digger client
+	} else {
+		// neither.... hmmmm... what to do now?
+		// for now lets just continue and pretend it didn't happen ok?
+	}
+
+	// TODO: get the entity-id of the user
 }
