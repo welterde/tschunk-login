9589f8746550f403f3560b53d7010124e01d166d
Tassilo Schweyer <dev@welterde.de>
2010-11-28 19:32:57 +0100

    more progress on the login handler

--- src/minecraft/handlers/login.go
+++ src/minecraft/handlers/login.go
@@ -14,6 +14,7 @@ func Handler(session *session.Session, msg packets.Packet) {
 	// check protocol version.. it should either be 5(minecraft client) or 7(manic digger)
 	if req.ProtocolVersion == 5 {
 		// minecraft client
+		// TODO: add more handlers
 	} else if req.ProtocolVersion == 7 {
 		// manic digger client
 	} else {
@@ -21,5 +22,32 @@ func Handler(session *session.Session, msg packets.Packet) {
 		// for now lets just continue and pretend it didn't happen ok?
 	}
 
-	// TODO: get the entity-id of the user
+	// check username... range is 1..42
+	if len(req.Username) < 1 || len(req.Username) > 42 {
+		// invalid username
+		// TODO: do something useful here?
+	}
+
+	// TODO: make sure that the username doesn't contain invalid characters
+
+	// ID of the player is /world/players/$username
+	id := "/world/players/" + req.Username
+
+	// TODO: perform authentication
+
+	// get the EID of this ID
+	eid := session.EntityManager.GetEntityID(id)
+
+	// send response
+	// TODO: what does MapSeed do?
+	// TODO: support multiple dimensions
+	// FIXME: is it uint32 or just int32? do we care?
+	resp := &packet.Response{
+		EID:       int32(eid),
+		MapSeed:   0,
+		Dimension: 0,
+	}
+
+	// now transmit the response
+	session.Transmit(resp)
 }
