f2c53a88142e8a3497289bd6cb3423810095f5fe
Tassilo Schweyer <dev@welterde.de>
2010-11-28 17:13:13 +0100

    log handshake

--- src/minecraft/handlers/handshake.go
+++ src/minecraft/handlers/handshake.go
@@ -1,5 +1,7 @@
 package handshake
 
+import log "log4go"
+
 import session "minecraft/session"
 import packets "minecraft/packets"
 import packet  "minecraft/packets/handshake"
@@ -8,16 +10,18 @@ import login_p "minecraft/packets/login"
 
 func Handler(session *session.Session, msg packets.Packet) {
 	// try to cast to an Request
-	_, ok := msg.(*packet.Request)
+	req, ok := msg.(*packet.Request)
 	if ok != true {
 		panic("BUG!!11!!1: failed cast to Request.. something is very messed up!")
 	}
 
-	// TODO: do an database lookup
+	// log handshake packet
+	log.Info("Got handshake<%s>", req.Username)
 
-	// yup.. answer handshake now
-	res := new(packet.Response)
-	res.Hash = "-"
+	// answer handshake
+	res := &packet.Response{
+		Hash: "-",
+	}
 
 	// now add login handler
 	session.SetHandler(login_p.REQ_PID, login.Handler)
